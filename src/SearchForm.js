fc25b3ca9faf9321c20b9a04ab991c08867efaff
ottakist <102596775+ottakist@users.noreply.github.com>
2022-09-25 00:05:54 +0300

    single movie commit

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useGlobalContext } from './context'
 const SearchForm = () => {
-  const {query,setQuery} = useGlobalContext()
+  const {query,setQuery,error} = useGlobalContext()
   return (<form className='search-form'>
     <h2>Search Movies</h2>
     <input type="text" className='form-input' value={query} onChange={(e)=>setQuery(e.target.value)}  />
+    {error.show && <div className="error">{error.msg}</div>}
   </form>)
 }
 
--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -4,12 +4,36 @@ import { API_ENDPOINT } from './context'
 
 const SingleMovie = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovie = async()=>{
-    
-  }
+  const [movie, setMovie] = useState([]);
   const {id}=useParams()
-  return <h2>{id}</h2>;
+  const getMovie = async(url)=>{
+    setIsLoading(true)
+    const response  = await fetch(url)
+    const data = await response.json()
+    setMovie(data)
+    setIsLoading(false)
+
+  }
+    useEffect(() => {
+      getMovie(`${API_ENDPOINT}&i=${id}`);
+    }, [id]);
+    const {Title:title,Plot:plot,Year:year,Poster:poster} = movie
+
+    
+  if (isLoading) {
+    return <div className='loading'></div>;
+  }
+  else{
+    return(<section className='single-movie'>
+      <img src={poster} alt={title} />
+      <div className="singke-movie-info">
+        <h2>{title}</h2>
+        <p>{plot}</p>
+        <h4>{year}</h4>
+        <Link className='btn' to={'/'}>Back to Movies</Link>
+      </div>
+    </section>)
+  }
 }
 
 export default SingleMovie
